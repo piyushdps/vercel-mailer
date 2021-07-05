51c29c6b514004ce03a511f3bf87af0db3de9c39
Piyush Aryan <piyusharyanofficial@gmail.com>
2021-07-05 15:48:55 +0530

    second commit

--- pages/api/index.js
+++ pages/api/index.js
@@ -11,12 +11,41 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 const sendEmail = require("./sendMail");
 
-const mailjet = require("node-mailjet").connect(
-  process.env.API_KEY,
-  process.env.SECRET
-);
 
-const jett = ("/api", function async (req, res, next) {
+const sendEmail =  require('./sendMail')
+
+import Cors from 'cors'
+
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+
+
+async function handler(req, res) {
+    // Run the middleware
+    await runMiddleware(req, res, cors)
+  
+    // Rest of the API logic
+   post(req,res)
+  }
+
+
+
+  const post = ("/api", function async (req, res, next) {
     const date = (new Date).getDate()
     const hour = (new Date).getHours()
     const minute = (new Date).getMinutes()
@@ -24,6 +53,14 @@ const jett = ("/api", function async (req, res, next) {
   res.status(200).json({ msg: date+hour+minute });
 
 });
+
+
+export default handler
+
+
+
+
+
 
 
 
